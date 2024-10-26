48423e3568ef4d0117fbe0340117aaf02e79c213
Jimmy Lee <jimmylee7890@gmail.com>
2024-10-26 21:07:14 +0800

    修正 jest 設定檔因為 time2fa 的 package.json 寫法導致 jest SyntaxError 的現象

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,9 @@ module.exports = {
   restoreMocks: true,
   coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  moduleNameMapper: {
+    // time2fa 的 package.json 他的 main 寫 ./dist/index.mjs
+    // 但是我的程式碼使用 require() ， jest 會抓錯，寫這行避免錯誤
+    '^time2fa$': '<rootDir>/node_modules/time2fa/dist/index.cjs',
+  },
 };
